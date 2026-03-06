ed0ec3de3436931b4e064a8bc26c640c197616c2
石井 伸弥 <ishii.shinya@fcs.ed.jp>
2026-03-07 06:30:10 +0900

    Add files via upload

--- src/components/simulations/YoungSim.jsx
+++ src/components/simulations/YoungSim.jsx
@@ -1,46 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { Link } from 'react-router-dom';
 
-// 指定波長(nm)からRGBカラーを近似的に算出する関数
-const getWavelengthRGB = (lambda) => {
-    let R, G, B;
-    if (lambda >= 380 && lambda < 440) {
-        R = -(lambda - 440) / (440 - 380);
-        G = 0.0;
-        B = 1.0;
-    } else if (lambda >= 440 && lambda < 490) {
-        R = 0.0;
-        G = (lambda - 440) / (490 - 440);
-        B = 1.0;
-    } else if (lambda >= 490 && lambda < 510) {
-        R = 0.0;
-        G = 1.0;
-        B = -(lambda - 510) / (510 - 490);
-    } else if (lambda >= 510 && lambda < 580) {
-        R = (lambda - 510) / (580 - 510);
-        G = 1.0;
-        B = 0.0;
-    } else if (lambda >= 580 && lambda < 645) {
-        R = 1.0;
-        G = -(lambda - 645) / (645 - 580);
-        B = 0.0;
-    } else if (lambda >= 645 && lambda <= 780) {
-        R = 1.0;
-        G = 0.0;
-        B = 0.0;
-    } else {
-        R = 0.0; G = 0.0; B = 0.0;
-    }
-    let intensity = 1.0;
-    if (lambda >= 380 && lambda < 420) intensity = 0.3 + 0.7 * (lambda - 380) / (420 - 380);
-    else if (lambda > 700 && lambda <= 780) intensity = 0.3 + 0.7 * (780 - lambda) / (780 - 700);
-
-    return {
-        r: Math.round(R * intensity * 255),
-        g: Math.round(G * intensity * 255),
-        b: Math.round(B * intensity * 255)
-    };
-};
+import { getWavelengthRGB } from '../../utils/color';
 
 // 共通の強度計算ロジック（表示幅 -20mm 〜 20mm の範囲について強度配列を返す）
 const calculateIntensities = (width, lambda, d, L, nMed, s, nPlate, N) => {
